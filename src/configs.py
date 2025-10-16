445d2820f276f536a9f709128f17eb9dbbd5ff73
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2025-10-16 11:14:05 +0330

    addressing the CUDA out of memory (1)

--- src/configs.py
+++ src/configs.py
@@ -105,19 +105,19 @@ class TrainingConfig:
     STAGE1_AUG_STRENGTH = 0.0 # No Mixup/CutMix during warm-up
 
     # --- Stage 2: Early Full Fine-Tuning ---
-    STAGE2_EPOCHS = 5
+    STAGE2_EPOCHS = 1
     STAGE2_BASE_LR = 1e-5 # Differential LR for backbone
     STAGE2_HEAD_LR = 1e-4 # Differential LR for head
     STAGE2_IMG_SIZE = 320
-    STAGE2_BATCH_SIZE = 32
+    STAGE2_BATCH_SIZE = 16
     STAGE2_AUG_STRENGTH = 0.2 # Mild Mixup/CutMix alpha
 
     # --- Stage 3: Final High-Resolution Polishing ---
-    STAGE3_EPOCHS = 13
+    STAGE3_EPOCHS = 1
     STAGE3_BASE_LR = 1e-6 # Lower LR for final polishing
     STAGE3_HEAD_LR = 1e-5
     STAGE3_IMG_SIZE = 384
-    STAGE3_BATCH_SIZE = 32 # May need to be reduced based on VRAM
+    STAGE3_BATCH_SIZE = 16 # May need to be reduced based on VRAM
     STAGE3_AUG_STRENGTH = 1.0 # Strong Mixup/CutMix alpha
     
     PIN_MEMORY = True
