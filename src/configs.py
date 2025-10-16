d0d955d1262d742ef1560a2e92af230734565407
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2025-10-16 10:04:58 +0330

    change the stage 3 epochs

--- src/configs.py
+++ src/configs.py
@@ -113,7 +113,7 @@ class TrainingConfig:
     STAGE2_AUG_STRENGTH = 0.2 # Mild Mixup/CutMix alpha
 
     # --- Stage 3: Final High-Resolution Polishing ---
-    STAGE3_EPOCHS = 30
+    STAGE3_EPOCHS = 20
     STAGE3_BASE_LR = 1e-6 # Lower LR for final polishing
     STAGE3_HEAD_LR = 1e-5
     STAGE3_IMG_SIZE = 384
