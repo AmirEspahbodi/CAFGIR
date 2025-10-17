9009dd7635f80d0ad724fa403bcd50b27ec5b490
a.espahbodi <dev.amir.espahbodi@gmail.com>
2025-10-17 13:05:02 +0330

    change batch sizes

--- src/configs.py
+++ src/configs.py
@@ -106,7 +106,7 @@ class TrainingConfig:
     STAGE1_EPOCHS = 4
     STAGE1_LR = 1e-3
     STAGE1_IMG_SIZE = 224
-    STAGE1_BATCH_SIZE = 32  # Can be larger due to smaller image size
+    STAGE1_BATCH_SIZE = 64  # Can be larger due to smaller image size
     STAGE1_ACCUMULATION_STEPS = 1
     STAGE1_AUG_STRENGTH = 0.0 # No Mixup/CutMix during warm-up
 
@@ -115,7 +115,7 @@ class TrainingConfig:
     STAGE2_BASE_LR = 1e-5 # Differential LR for backbone
     STAGE2_HEAD_LR = 1e-4 # Differential LR for head
     STAGE2_IMG_SIZE = 320
-    STAGE2_BATCH_SIZE = 8
+    STAGE2_BATCH_SIZE = 16
     STAGE2_ACCUMULATION_STEPS = 4
     STAGE2_AUG_STRENGTH = 0.2 # Mild Mixup/CutMix alpha
 
@@ -124,7 +124,7 @@ class TrainingConfig:
     STAGE3_BASE_LR = 1e-6 # Lower LR for final polishing
     STAGE3_HEAD_LR = 1e-5
     STAGE3_IMG_SIZE = 384
-    STAGE3_BATCH_SIZE = 4
+    STAGE3_BATCH_SIZE = 8
     STAGE3_ACCUMULATION_STEPS = 8
     STAGE3_AUG_STRENGTH = 1.0 # Strong Mixup/CutMix alpha
     
